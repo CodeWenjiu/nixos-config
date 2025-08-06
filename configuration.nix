fabb4411b738ec8644d10a1eaeae93d3a39f85b9
wenjiu <2784307979@qq.com>
2025-08-06 16:42:55 +0800

    feat: net tools

--- configuration.nix
+++ configuration.nix
@@ -153,7 +153,9 @@ in {
 
     git lazygit
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    
     wget
+    nettools
   ];
 
   programs.git = {
