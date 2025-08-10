1b7c9538c0394ef33bc92db64db118904ba87945
wenjiu <2784307979@qq.com>
2025-08-10 12:03:39 +0800

    feat: waybar

--- home/hypr/hypr.nix
+++ home/hypr/hypr.nix
@@ -1,6 +1,6 @@
 { config, pkgs, lib, ... }:
 let
-  statusBar = "hyprpanel";
+  statusBar = "waybar";
   
   hyprlandConfig = pkgs.substituteAll {
     src = ./hyprland.conf.template;
--- home/hypr/status_bar/waybar.nix
+++ home/hypr/status_bar/waybar.nix
@@ -3,4 +3,6 @@
   home.packages = with pkgs; [
     waybar
   ];
+  
+  xdg.configFile."waybar".source = ./waybar;
 }
