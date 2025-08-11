4a751537051b20e0469bafab4d98bc80183876cc
wenjiu <2784307979@qq.com>
2025-08-11 11:28:31 +0800

    chore: kitty setup

--- home/modules/terminal/terminal.nix
+++ home/modules/terminal/terminal.nix
@@ -8,13 +8,20 @@
         nettools
     ];
 
+    # see also https://discourse.nixos.org/t/configure-kitty-with-home-manager/57505/2
     programs.kitty = {
         enable = true;
         package = pkgs.kitty;
 
+        settings = {
+            background_opacity = "0.7";
+            confirm_os_window_close = 0;
+            background_blur = "1";
+        };
+        
         extraConfig = ''
-        map ctrl+c copy_and_clear_or_interrupt
-        map ctrl+v paste_from_clipboard
+            map ctrl+c copy_and_clear_or_interrupt
+            map ctrl+v paste_from_clipboard
         '';
     };
 
