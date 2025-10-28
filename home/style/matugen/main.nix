ecb7bb07ac63ec8a4a397148e2872ef6fbad28eb
wenjiu <2784307979@qq.com>
2025-10-28 14:58:29 +0800

    chore(theme): gtk and qt theme

--- home/style/matugen/main.nix
+++ home/style/matugen/main.nix
@@ -10,6 +10,24 @@
 
   xdg.configFile."matugen".source = ./config;
 
+  xdg.configFile."gtk-3.0/gtk.css".text = ''
+    @import 'colors.css';
+  '';
+  xdg.configFile."gtk-4.0/gtk.css".text = ''
+    @import 'colors.css';
+  '';
+
+  xdg.configFile."qt5ct/qt5ct.conf".text = ''
+    [Appearance]
+    color_scheme_path=wenjiu/.config/qt5ct/colors/matugen.conf
+    custom_palette=true
+  '';
+  xdg.configFile."qt6ct/qt6ct.conf".text = ''
+    [Appearance]
+    color_scheme_path=wenjiu/.config/qt6ct/colors/matugen.conf
+    custom_palette=true
+  '';
+
   xdg.configFile."ghostty/config".text = ''
     theme = matugen
   '';
