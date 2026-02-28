1421125bbab838a751d31ff12a196c621c44526a
wenjiu <2784307979@qq.com>
2026-02-28 14:27:40 +0800

    chore

--- home/desktop/noctalia/main.nix
+++ home/desktop/noctalia/main.nix
@@ -55,10 +55,11 @@
                         }
                         {
                             id = "SystemMonitor";
-                            showCpuFreq = false;
-                            showCpuTemp = false;
-                            showCpuUsage = false;
-                            showMemoryUsage = false;
+                            # showCpuFreq = false;
+                            # showCpuTemp = false;
+                            # showCpuUsage = false;
+                            # showMemoryUsage = false;
+                            showMemoryAsPercent = true;
                             showNetworkStats = true;
                         }
                         {
@@ -425,27 +426,6 @@
                                 showBackground = true;
                                 x = 1360;
                                 y = 816;
-                            }
-                            {
-                                diskPath = "/";
-                                id = "SystemStat";
-                                layout = "bottom";
-                                roundedCorners = true;
-                                showBackground = true;
-                                statType = "CPU";
-                                x = 68;
-                                y = 34;
-                            }
-                            {
-                                diskPath = "/";
-                                id = "SystemStat";
-                                layout = "bottom";
-                                roundedCorners = true;
-                                scale = 1;
-                                showBackground = true;
-                                statType = "Memory";
-                                x = 68;
-                                y = 170;
                             }
                         ];
                     }
