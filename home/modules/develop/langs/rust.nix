fd24e79c4f8abea2a20e70909107eaf7c28840aa
wenjiu <2784307979@qq.com>
2026-03-03 19:14:17 +0800

    chore: bump dependencies

--- home/modules/develop/langs/rust.nix
+++ home/modules/develop/langs/rust.nix
@@ -1,18 +1,17 @@
 {
-  pkgs,
   ...
 }:
 {
-  home.packages = with pkgs; [
-    (rust-bin.stable.latest.default.override {
-      extensions = [
-        "rust-src"
-        "rust-analyzer"
-      ];
-      targets = [
-        "wasm32-unknown-unknown"
-      ];
-    })
-    cargo-generate
+  home.packages = [
+    # (rust-bin.stable.latest.default.override {
+    #   extensions = [
+    #     "rust-src"
+    #     "rust-analyzer"
+    #   ];
+    #   targets = [
+    #     "wasm32-unknown-unknown"
+    #   ];
+    # })
+    # cargo-generate
   ];
 }
